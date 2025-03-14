b6f951473c008bbb3b1a6cdecdab7f904d7cc510
khloé.kim <jinbiue_@naver.com>
2025-03-14 21:25:12 +0900

    Test: debug

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage, SystemMessage
@@ -11,21 +12,3 @@ messages = [
     SystemMessage("Translate the following from English into Italian"),
     HumanMessage("hi!"),
 ]
-
-# print(model.invoke(messages))
-
-# for token in model.stream(messages):
-#     print(token.content, end="|")
-
-from langchain_core.prompts import ChatPromptTemplate
-
-system_template = "Translate the following from English into {language}"
-
-prompt_template = ChatPromptTemplate.from_messages(
-    [("system", system_template), ("user", "{text}")]
-)
-
-prompt = prompt_template.invoke({"language": "Italian", "text": "hi!"})
-
-response = model.invoke(prompt)
-print(response.content)
