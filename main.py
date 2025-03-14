7b70f0a8ba177a64df175b274679c332cfcd8878
khloé.kim <jinbiue_@naver.com>
2025-03-14 20:46:01 +0900

    Test: Debug

--- main.py
+++ main.py
@@ -1,52 +1,31 @@
-import pickle
-from typing import Union
-from fastapi import FastAPI
-# model.py 가져옴
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, SystemMessage
 
-import model
-
-
-app = FastAPI()
-
-# 모델 저장 경로
-MODEL_PATH = "saved_model.pkl"
-
-model = model.AndModel()
-
-@app.get("/")
-def read_root():
-    #딕셔너리를 반환하면 JSON으로 응답한다.
-    return {"Hello": "World"} 
-
-#/items/{item_id} 경로
-# item_id 경로 매개변수(파라메터)
-@app.get("/items/{item_id}") # EendPoint
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-@app.get("/predict/left/{left}/right/{right}")
-def predict(left: int, right : int):
-    result = model.predict([left, right])
-    return {"result": result }
+load_dotenv()
+model = init_chat_model("gpt-4o-mini", model_provider="openai")
 
 
-@app.post("/train")
-def train():
-    model.train()
-    return {"result": "OK"}
 
-@app.post("/save")
-def save():
-    # 파일로 저장
-    with open('saved_model.pkl', 'wb') as f:
-        pickle.dump(model, f)
-    return {"Saved": "OK"}
+messages = [
+    SystemMessage("Translate the following from English into Italian"),
+    HumanMessage("hi!"),
+]
 
+# print(model.invoke(messages))
 
-@app.post("/load")
-def load():
-    global model  # ✅ FastAPI 앱의 글로벌 model 변수 사용
-    with open('saved_model.pkl', 'rb') as f:
-        model = pickle.load(f)
-        return {"Loaded": "OK"}
-    
+# for token in model.stream(messages):
+#     print(token.content, end="|")
+
+from langchain_core.prompts import ChatPromptTemplate
+
+system_template = "Translate the following from English into {language}"
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", "{text}")]
+)
+
+prompt = prompt_template.invoke({"language": "Italian", "text": "hi!"})
+
+response = model.invoke(prompt)
+print(response.content)
